3a4d4feb82edf180c3fc6c7b062b0b42edc79477
filipeavila7 <filipeavila076@gmail.com>
2026-06-08 21:39:16 -0300

    iniciando configurações de security e jwt

--- src/main/java/com/example/gerenciador/jwt/JwtService.java
+++ src/main/java/com/example/gerenciador/jwt/JwtService.java
@@ -1,5 +1,22 @@
 package com.example.gerenciador.jwt;
 
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+
+@Service
 public class JwtService {
+    private static final String SECRET =
+            "sua-chave-super-secreta-com-pelo-menos-32-caracteres";
+
+
+    private Key getKey(){
+        return Keys.hmacShaKeyFor(
+                SECRET.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+
 }
